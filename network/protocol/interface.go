f9a1d0f534b3c4c7d44c232e6076c527441a017b
wizegene <ncloutier@wizegene.com>
2021-02-28 17:44:56 -0500

    commit

--- network/protocol/interface.go
+++ network/protocol/interface.go
@@ -1,1 +1,27 @@
 package protocol
+
+import (
+	"WizechainFoundation/wizechain/network"
+	"WizechainFoundation/wizechain/network/identity"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
+type MemberProvider interface {
+	GetMembers() []identity.ID
+}
+
+type DefaultMembersProvider struct {
+	WLP network.WhiteListProvider
+}
+
+// GetMembers return the list of all the members
+func (d *DefaultMembersProvider) GetMembers() []identity.ID {
+	pis, _ := d.WLP.GetPeers()
+	members := make([]identity.ID, len(pis))
+	for index, b := range pis {
+		pi := peer.AddrInfo{}
+		_ = pi.UnmarshalJSON(b)
+		members[index] = identity.ID(pi.ID)
+	}
+	return members
+}
