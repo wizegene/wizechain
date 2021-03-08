917b9f80c498ac081b9a5d838265d897c12491ca
wizegene <ncloutier@wizegene.com>
2021-03-08 12:05:53 -0500

    first

--- chaincore/core/dogapi.go
+++ chaincore/core/dogapi.go
@@ -9,7 +9,7 @@ import (
 const DOGE_TESTNET_API_KEY = "571b-3bd9-8808-a3bd"
 const TEST_API_BALANCE_URL = "https://block.io/api/v2/get_address_balance/?api_key=" + DOGE_TESTNET_API_KEY + "&labels=testwize"
 const TEST_API_EST_NETFEE = "https://block.io/api/v2/get_network_fee_estimate/?api_key=" + DOGE_TESTNET_API_KEY
-const NOMIC = "e427d4424106ecb2d3ce25276c128077"
+const NOMIC = ""
 const TICKER_URL = "https://api.nomics.com/v1/currencies/ticker?key=" + NOMIC + "&ids=DOGE&interval=1d,30d&convert=USD&per-page=1&page=1"
 
 type DOGEInfo struct {
