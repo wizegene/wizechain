22e55c4a3d070242b6ecefc48b2d737599371712
wizegene <ncloutier@wizegene.com>
2021-03-14 00:12:50 -0500

    revised genesis payload and block

--- chaincore/core/genesis.go
+++ chaincore/core/genesis.go
@@ -5,17 +5,18 @@ import (
 	"errors"
 	"github.com/beevik/ntp"
 	"github.com/rogpeppe/fastuuid"
+	"github.com/wizegene/wizechain/chaincore/config"
 	"io/ioutil"
 )
 
 type Genesis struct {
-	GenesisTime int64          `json:"genesis_time"`
-	ChainID     string             `json:"chain_id"`
-	Balances    map[string]float32 `json:"balances"`
-	Currency    *CurrencyConfig    `json:"currency"`
-	POS *POSParams	`json:"pos_parameters"`
-	BlockchainParams *BlocksParams `json:"blocks_parameters"`
-	inBlock *Block
+	GenesisTime      int64              `json:"genesis_time"`
+	ChainID          string             `json:"chain_id"`
+	Balances         map[string]float32 `json:"balances"`
+	Currency         *CurrencyConfig    `json:"currency"`
+	POS              *POSParams         `json:"pos_parameters"`
+	BlockchainParams *BlocksParams      `json:"blocks_parameters"`
+	inBlock          []byte
 }
 
 type CurrencyConfig struct {
@@ -28,15 +29,26 @@ func LoadGenesisFromJSON() *Genesis {
 	var G Genesis
 	g, _ := ioutil.ReadFile("./config/genesis.json")
 
-	json.Unmarshal(g, &G)
+	_ = json.Unmarshal(g, &G)
 	return &G
+
+}
+
+func WriteGenesisToFile(data *Genesis) {
+	ser, _ := json.Marshal(data)
+	_ = ioutil.WriteFile("./config/genesis.json", ser, 0644)
+
+}
+
+func WriteInBlockToFile(blockData []byte) {
+	_ = ioutil.WriteFile("./config/in_block.json", blockData, 0644)
 
 }
 
 func CreateGenesisBlock() (*Genesis, error) {
 
 	g := &Genesis{}
-	b := &Block{}
+
 	chainID, err := fastuuid.NewGenerator()
 	if err != nil {
 		return nil, err
@@ -44,9 +56,12 @@ func CreateGenesisBlock() (*Genesis, error) {
 	// new randomize uuid 192 bit as hex 128 bit string
 	cid := chainID.Hex128()
 	//we create a blank block to be associated to the genesis content
-	b.CreateBlankBlock(cid, nil, uint32(nil), uint32(1))
 
-	blockTime := b.Created
+	bser := CreateNewBlock(cid, nil, uint32(0), uint32(1))
+	var block Block
+	json.Unmarshal(bser, &block)
+
+	blockTime := block.Created
 	timestamp, _ := ntp.Time("pool.ntp.org")
 	if blockTime > timestamp.UnixNano() {
 		//invalid time
@@ -59,11 +74,29 @@ func CreateGenesisBlock() (*Genesis, error) {
 
 	g.GenesisTime = timestamp.UnixNano()
 	g.ChainID = cid
-	g.Balances = make( map[string]float32 )
+	g.Balances = make(map[string]float32)
 	g.Currency = &CurrencyConfig{}
 	g.POS = &POSParams{}
 	g.BlockchainParams = &BlocksParams{}
-	g.inBlock = b
+	g.inBlock = bser
+
+	WriteInBlockToFile(bser)
+
+	g.Currency.Name = config.Currency
+	g.Currency.MaxSupply = -1
+	g.BlockchainParams.CoinsPerBlock = int32(config.CoinsPerBlock)
+	g.BlockchainParams.isInflationary = true
+	g.BlockchainParams.MaxBlockPerYear = float32(config.MaxBlockPerYear)
+	g.BlockchainParams.MaxSecPerBlock = int64(config.SecondsBetweenBlocks)
+	g.POS = &POSParams{}
+	g.POS.MaxValidators = config.MaxValidators
+	g.POS.MinValidators = config.MinValidators
+	g.POS.InflationaryRate = config.RewardInflationPerSecond
+	g.POS.StartingRewardPerValidator = config.StartRewardPerValidator
+	g.POS.StartingRewardAsCurrency = config.StartRewardAsWize
+	g.POS.Quorum = float32(config.Corum)
+
+	WriteGenesisToFile(g)
 
 	return g, nil
 
@@ -76,25 +109,23 @@ func (g *Genesis) toJSON() []byte {
 
 }
 
-
-
 type POSParams struct {
-	MinValidators int
-	MaxValidators int
-	Quorum float32
-	MaxValidatorPower float64
-	ChangeLeaderFreq float64
+	MinValidators              int
+	MaxValidators              int
+	Quorum                     float32
+	MaxValidatorPower          float64
+	ChangeLeaderFreq           float64
 	StartingRewardPerValidator float64
-	StartingRewardAsCurrency float64
-	InflationaryRate float64
-	DeflationaryRate float64
+	StartingRewardAsCurrency   float64
+	InflationaryRate           float64
+	DeflationaryRate           float64
 }
 
 type BlocksParams struct {
 	MaxBlockPerYear float32
-	CoinsPerBlock int32
+	CoinsPerBlock   int32
 	MaxCoinsPerYear float64
-	isDeflationary bool
-	isInflationary bool
-	MaxSecPerBlock int64
+	isDeflationary  bool
+	isInflationary  bool
+	MaxSecPerBlock  int64
 }
